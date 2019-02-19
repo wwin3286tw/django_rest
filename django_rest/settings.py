75b025fa8e9219c0db98f881015cfc6ee1d01ae0
Yi-Jia,Ye <wwin3286tw@yahoo.com.tw>
2019-02-19 16:53:51 +0800

    測試第一個django API APP功能

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o7v&6$t!$7pmbi*f+0z(&%6nd9!hb+y3$!kjo6bp6s(i-)775g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'musics',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'django_rest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR + '/templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
