ec37f5a3e5e6a7cc4ba2f711c53c958e60a58b32
Yi-Jia,Ye <wwin3286tw@yahoo.com.tw>
2019-02-19 22:48:46 +0800

    Fix settings.py for CI environment

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -118,8 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-import django_heroku
-django_heroku.settings(locals())
-
-
+STATIC_URL = '/static/' 
+if 'HEROKU' in os.environ:
+    import django_heroku
+    django_heroku.settings(locals())
