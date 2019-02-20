b236e7b148a553cd248fd183ce4658843fc36ed6
Yi-Jia,Ye <wwin3286tw@yahoo.com.tw>
2019-02-20 11:29:01 +0800

    加入API功能

--- django_rest/urls.py
+++ django_rest/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from musics.views import hello_view
+from django.conf.urls import url, include
+
+#from musics.views import hello_view
+#from musics.views import root_view
+from rest_framework.routers import DefaultRouter
+from musics import views
+
+router = DefaultRouter()
+router.register(r'music', views.MusicViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_view),
+    path('api/', include(router.urls))
 ]
--- musics/views.py
+++ musics/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+# Create your views here.
+from musics.models import Music
+from musics.serializers import MusicSerializer
+
+from rest_framework import viewsets
+
 
 # Create your views here.
-def hello_view(request):
-    return render(request, 'hello_django.html', {
-        'data': "Hello Django ",
-    })
+class MusicViewSet(viewsets.ModelViewSet):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
