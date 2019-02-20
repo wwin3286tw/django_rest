3aeccb0bc25419b37f52de0c3c07387ce37fc0ce
Yi-Jia,Ye <wwin3286tw@yahoo.com.tw>
2019-02-20 18:50:43 +0800

    Update API

--- joystick_api/views.py
+++ joystick_api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from joystick_api.models import User
 from joystick_api.serializers import JAPI_Serializer
+from rest_framework import viewsets, mixins
 
-from rest_framework import viewsets
-
+#from rest_framework import viewsets
 
 # Create your views here.
-class APIViewSet(viewsets.ModelViewSet):
+class APIViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.ListModelMixin,viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = JAPI_Serializer
